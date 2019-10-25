4d2d99f9f13992d7a15cf6a878dad39eb40eb355
chukwuemeka.mbachu <victor.c.mbachu@gmail.com>
2019-10-25 13:51:34 +0100

    finished song favorite function

--- music/views.py
+++ music/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, Http404
 # from django.template import loader
-from .models import Album
+from .models import Album, Song
 
 
 # Create your views here.
@@ -42,3 +42,27 @@ def detail(request, album_id):
     album = get_object_or_404(Album, pk=album_id)
     context = {'album': album}
     return render(request, 'music/detail.html', context)
+
+
+def favorite(request, album_id):
+    album = get_object_or_404(Album, pk=album_id)
+    try:
+        selected_song = album.song_set.get(pk=request.POST['song'])
+    except(KeyError, Song.DoesNotExist):
+        context = {
+            'album': album,
+            'error_message': 'You did not select any song',
+        }
+        return render(request, 'music/detail.html', context)
+    else:
+        if selected_song.is_favorite:
+            selected_song.is_favorite = False
+            selected_song.save()
+        else:
+            selected_song.is_favorite = True
+            selected_song.save()
+
+        context = {
+            'album': album
+        }
+        return render(request, 'music/detail.html', context)
